c66f18c47f681df5052db88e21110d4a92032eca
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2019-03-27 10:01:01 -0400

    Added Prims code

--- src/CCC/CCC_2010_S4.java
+++ src/CCC/CCC_2010_S4.java
@@ -57,41 +57,103 @@ import java.util.Scanner;
 //}
 public class CCC_2010_S4 {
 
-	
+	import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
+public class Main {
 	public static void main(String[] args) {
+		new Main();
+	}
 
-		Scanner s = new Scanner(System.in);
+	public Main() {
+		Scanner scanner = new Scanner(System.in);
+		int penNum = scanner.nextInt();
 
-		int n = s.nextInt();
+		Vertex[] pens = new Vertex[penNum];
 
-		Vertex[] pen = new Vertex[n];
-		HashMap<Point, Edge> key = new HashMap<>();
-		for(int i = 0; i < n ; i++){
-			pen[i] = new Vertex(i);
-		}
-		
-		for (int i = 0; i < n; i++) {
-			int e = s.nextInt();
-			int[] points = new int[e];
-			for (int j = 0; j < e; j++) {
+		HashMap<Point, Edge> key = new HashMap<Point, Edge>();
+		for (int i = 0; i < penNum; i++)
+			pens[i] = new Vertex(i);
+		for (int i = 0; i < penNum; i++) {
+			int edges = scanner.nextInt();
+			int[] points = new int[edges];
+			for (int j = 0; j < edges; j++) {
+				points[j] = scanner.nextInt();
+			}
+			for (int j = 0; j < edges; j++) {
 				int u;
-				if (j == e - 1) {
+				if (j == edges - 1)
 					u = 0;
-				} else {
+				else
 					u = j + 1;
-				}
-				if (key.containsKey(new Point(points[j], points[u]))) {
-
-				} else if (key.containsKey(new Point(points[j], points[u]))) {
-
+				if (key.containsKey(new Point(points[j], points[u]))
+						|| key.containsKey(new Point(points[u], points[j]))) {
+					Edge e = key.containsKey(new Point(points[j], points[u]))
+							? key.remove(new Point(points[j], points[u])) : key.remove(new Point(points[u], points[j]));
+					e.to = pens[i];
+					Edge e2 = new Edge(scanner.nextInt());
+					e2.to = e.from;
+					e2.from = pens[i];
+					pens[i].addEdge(e2);
 				} else {
-
+					Edge e = new Edge(scanner.nextInt());
+					e.from = pens[i];
+					key.put(new Point(points[j], points[u]), e);
+					pens[i].addEdge(e);
 				}
 			}
-
 		}
-
+		Vertex outside = new Vertex(-1);
+		Iterator<Edge> i = key.values().iterator();
+		while (i.hasNext()) {
+			Edge e = i.next();
+			e.to = outside;
+			Edge e2 = new Edge(e.cost);
+			e2.from = outside;
+			e2.to = e.from;
+			outside.addEdge(e2);
+		}
+		System.out.println(prim(pens[0], penNum + 1, outside));
+		scanner.close();
 	}
+
+	public int prim(Vertex origin, int count, Vertex outside) {
+		//http://algs4.cs.princeton.edu/43mst/
+		int cost = 0;
+		int outsideCount = 0;
+		int outsideVal = 0;
+		PriorityQueue<Edge> next = new PriorityQueue<Edge>();
+		HashSet<Vertex> visited = new HashSet<Vertex>();
+		next.addAll(origin.edges);
+		visited.add(origin);
+		while (true) {
+			if (visited.size() == count)
+				break;
+			Edge e = next.poll();
+			if (visited.contains(e.to))
+				continue;
+			else {
+				next.addAll(e.to.edges);	// automatically sorted
+				visited.add(e.to);// you can also save the edge e to the result
+				cost += e.cost;
+				if (e.from == outside || e.to == outside) {
+					outsideCount++;
+					if (outsideVal == 0)
+						outsideVal = e.cost;
+				}
+			}
+		}
+		if (outsideCount > 1)
+			return cost;
+		else
+			return cost - outsideVal;
+	}
+}
+
 
 }
